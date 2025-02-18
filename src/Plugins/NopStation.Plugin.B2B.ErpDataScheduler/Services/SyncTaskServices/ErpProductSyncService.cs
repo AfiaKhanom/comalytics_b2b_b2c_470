1bb0bf88f5004058617d537aa9bb0c533f2238de
Md. Anwarul Habib <anwarul.habib@brainstation-23.com>
2025-02-18 15:40:43 +0000

    2933 - Removed the wrong property mapping of delete property from product sync.

--- src/Plugins/NopStation.Plugin.B2B.ErpDataScheduler/Services/SyncTaskServices/ErpProductSyncService.cs
+++ src/Plugins/NopStation.Plugin.B2B.ErpDataScheduler/Services/SyncTaskServices/ErpProductSyncService.cs
@@ -425,7 +425,6 @@ public class ErpProductSyncService : IErpProductSyncService
                                 oldErpProduct.TaxCategoryId = allTaxCategories.FirstOrDefault(x => x.Name.Equals(erpProduct.TaxCategoryName))?.Id ?? 0;
                             }
 
-                            oldErpProduct.Deleted = erpProduct.Published;
                             oldErpProduct.Published = erpProduct.Published;
                             oldErpProduct.Deleted = false;
                             oldErpProduct.CreatedOnUtc = DateTime.UtcNow;
@@ -489,7 +488,6 @@ public class ErpProductSyncService : IErpProductSyncService
                                 oldErpProduct.TaxCategoryId = allTaxCategories.FirstOrDefault(x => x.Name.Equals(erpProduct.TaxCategoryName))?.Id ?? 0;
                             }
 
-                            oldErpProduct.Deleted = erpProduct.Published;
                             oldErpProduct.Published = erpProduct.Published;
                             oldErpProduct.UpdatedOnUtc = DateTime.UtcNow;
 
